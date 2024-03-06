995d9ee25dae210f4a6e8be1c1f3a5d620cd4409
Warren Atwiine <150776835+atwiinewarren@users.noreply.github.com>
2024-03-06 22:52:41 +0300

    1. Put the Enums in their own class and folder 2. Transferred the methods related to a teacher to the teacher class 3. Fixed the object instantiation logic

---
+++ CalculateTeacher'sSalary/Enums/Grades.cs
@@ -0,0 +1,15 @@
+﻿namespace CalculateTeacher_sSalary;
+
+public enum Grades
+{
+    grade1 = 1,
+    grade2 = 2,
+    grade3 = 3,
+    grade4 = 4,
+    grade5 = 5,
+    grade6 = 6,
+    grade7 = 7,
+    grade8 = 8
+}
+
+
---
+++ CalculateTeacher'sSalary/Enums/MaritalStatus.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace CalculateTeacher_sSalary;
+
+public enum MaritalStatus
+{
+    married = 0,
+    single = 1,
+    widow = 2,
+    widower = 3,
+}
+
